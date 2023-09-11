f7b2c23e6b8bccf34009488ba004037429e51be2
Lise <35637366+luararamos@users.noreply.github.com>
2023-09-11 19:26:49 -0300

    statement

---
+++ app/src/main/java/com/example/mymoneyapp/statement/MainViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.mymoneyapp.statement
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    private var txtTotalValue = MutableLiveData<String>()
+    private var vi = MutableLiveData<Boolean>()
+
+    init {
+        txtTotalValue.value = "10.000,00"
+    }
+
+    fun totalValue() : LiveData<String>{
+        return txtTotalValue
+    }
+}
